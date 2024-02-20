1c251a07f299b97100fc75405eb9913df724dbee
yzkar <160213905+yzkar@users.noreply.github.com>
2024-02-20 09:40:40 +0800

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,8 +9,8 @@ class Home extends BaseController
         return view('welcome_message');
     }
 
-    public function coba()
-    {
-        echo $this->name;
-    }
+    // public function coba()
+    // {
+    //     echo $this->name;
+    // }
 }
